b1233e235320b015ea42a9bfe3b8efe77d7dab03
JV2008 <joaovictor25032@gmail.com>
2024-08-14 14:33:27 -0300

    primeiro commit

---
+++ DeclaraçãoLong.java
@@ -0,0 +1,15 @@
+package atividades;
+
+public class DeclaraçãoLong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nome = "JOÃO VICTOR";
+		long cpf = 47808807850l;
+		
+		System.out.println(nome);
+		System.out.println(cpf);
+	}
+
+}
---
+++ Var.java
@@ -0,0 +1,16 @@
+package atividades;
+
+public class Var {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		var f = 109;
+		final var FC = 32;
+		final var CC = 5.0/9.0;
+		
+		var c = ((f - FC)*(CC));
+		
+		System.out.println("A temperatura em fahrenheit é "+ f + " A temperatura em celcius é " + c +".");
+	}
+
+}
